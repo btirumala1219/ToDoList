c62cab225fcda4d95c0ff729353711537bedc4d9
btirumala <barath.tirumala@gmail.com>
2018-03-14 22:02:31 -0400

    Design and implement processes for dynamically changing the content of the list

---
+++ js/main.js
@@ -0,0 +1,84 @@
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveTask);
+
+function saveTask(e){
+  console.log("savetask is called");
+  var taskName =document.getElementById('taskName').value;
+  var taskDetails =document.getElementById('taskDetails').value;
+
+
+ var task = {
+   name: taskName,
+   details: taskDetails
+ }
+
+ if(localStorage.getItem('tasks') === null){
+
+    var tasks = [];
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+
+    var tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  }
+
+  document.getElementById('myForm').reset();
+  fetchTasks();
+
+  //e.preventDefault();
+
+}
+
+function deleteTask(url){
+
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  for(var i =0;i < tasks.length;i++){
+
+    if(tasks[i].name == name){
+
+      tasks.splice(i, 1);
+
+    }
+
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  fetchtasks();
+
+}
+
+function fetchTasks(){
+
+  var tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  var tasksResults = document.getElementById('tasksResults');
+
+  tasksResults.innerHTML = '';
+  console.log(tasks);
+  if(tasks != null){
+    for(var i = 0; i < tasks.length ; i++){
+
+    var name = tasks[i].name;
+    var details = tasks[i].details;
+
+    tasksResults.innerHTML += '<div class="well">'+
+
+                                  '<h3>'+name+ '<p>' + details + '</p>'
+
+                                  '<p>'+'<a onclick="deleteTask(\''+name+'\')" class="btn btn-danger" href="#">Delete</a>' + '</p>' +
+
+                                  '</h3>'+
+
+                                  '</div>';
+}
+  }
+
+}
+
+
+
+// Validate Form
